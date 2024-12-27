9a853075739ebe085c10ee711bdc2961c43833e9
sahana <sahanavuyyala03@gmail.com>
2024-12-27 10:10:35 -0600

    featurefiles

---
+++ src/main/java/utilities/Allfeaturefiles.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Allfeaturefiles {
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		String excelsheetpath = "C:\\Sahana NUMPY NINJA SDET\\Questers_DsAlgo_Scenarios.xlsx";
+		
+		FileInputStream fis = new FileInputStream(excelsheetpath);
+		
+		
+		
+		
+		
+	}
+
+}
